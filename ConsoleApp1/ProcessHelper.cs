4f7c5b3eeae346d195ec69d49792a51284e40a8e
Robot144Robotovitz <ipshubin144@gmail.com>
2018-12-18 22:55:28 +0300

    added processHelper and writeHelper

---
+++ ConsoleApp1/ProcessHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class ProcessHelper
+    {
+        public static Process getProcessFromName(Config config)
+        {
+            Process p = null;
+            Process[] processlist = Process.GetProcessesByName(config.GetProcessName());
+            foreach (Process theprocess in processlist)
+            {
+                //yet again, should use file log here
+                Console.WriteLine("Process: {0} ID: {1}", theprocess.ProcessName, theprocess.Id);
+                p = theprocess;
+            }
+            return p;
+        }
+
+        public static void waitForProcess(Config config)
+        {
+            //we don't want to wait forever, so let's say we'll wait for ~1 minute
+            //should i use a Timer instaed?
+            int count = 0;
+           
+            while (!isProcessAlive(config)) {
+                count++;
+                Thread.Sleep(50);
+                if (count > 3600) throw new Exception("Process has not started in ... minutes. Exiting.");
+
+            }
+            //return true;
+        }
+
+        public static bool isProcessAlive(Config config ) {
+            Process[] processlist = Process.GetProcessesByName(config.GetProcessName());
+            if (processlist.Length == 0) return false;
+            else return true;
+        }
+
+    }
+}
---
+++ ConsoleApp1/writeHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class writeHelper
+    {
+        public static void Write(Config config, ArrayList result)
+        {
+            //we can do different sorts of actions based on config:
+            //write in csv with comma, or semicolon, or json, or watnot
+            using (System.IO.StreamWriter file =
+        new System.IO.StreamWriter(config.GetoOutputFile() + GetFileName(config), true))
+            {
+                //TODO: refactor with getNames from Result
+                file.WriteLine("% Processor Time; Working Set(RAM); Handle Count; Thread Count");
+                foreach (Result line in result) {
+                    var res = string.Format("{0}; {1}; {2}; {3}", line.getCpuCounter(), line.getRamCounter(), line.getPageCounter(), line.getThreadCounter());
+                    file.WriteLine(res);
+                    file.Flush();
+                }
+                
+            }
+
+        }
+
+        private static string GetFileName(Config config) {
+            string mimeType = ".csv";
+            string fileName = Path.DirectorySeparatorChar + "result" + DateTime.Now.ToString("yyyyMMddHHmmssffff") + mimeType;
+            return fileName;
+        }
+    }
+}
